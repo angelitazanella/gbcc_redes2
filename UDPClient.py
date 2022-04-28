2f009c2a99c9158f7d95e82f1bfbd8307ae8699e
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2022-04-28 08:29:44 -0300

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -1,12 +1,11 @@
 from socket import *
+
 serverName = "hostname"
-
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input("Informe uma frase em minúsculas:")
 clientSocket.sendto(message.encode(),(serverName, serverPort))
-
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
