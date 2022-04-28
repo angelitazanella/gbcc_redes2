7ef1760dd76e3afb6fd05ed701549794e88c9457
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2022-04-28 08:28:07 -0300

    Update TCPServer.py

--- TCPServer.py
+++ TCPServer.py
@@ -2,10 +2,10 @@
 
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((‘’,serverPort))
+serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
 
-print ‘Aguardando conexões’
+print("Aguardando conexões")
 
 while True:
      connectionSocket, addr = serverSocket.accept()
