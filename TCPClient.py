295c939dcd69e9e7f9960d65827dc5d5864387c0
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2022-04-28 08:24:01 -0300

    Create TCPClient.py

---
+++ TCPClient.py
@@ -0,0 +1,19 @@
+from socket import *​
+
+serverName = ’servername’​
+
+serverPort = 12000​
+
+clientSocket = socket(AF_INET, SOCK_STREAM)​
+
+clientSocket.connect((serverName,serverPort))​
+
+sentence = raw_input(‘Informe uma frase em minúsculas:’)​
+
+clientSocket.send(sentence.encode())​
+
+modifiedSentence = clientSocket.recv(1024)​
+
+print (‘Resposta do servidor:’, modifiedSentence.decode())​
+
+clientSocket.close()
